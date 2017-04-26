37de3f2e4df1fb5d2887feea1d41b57d52ef986f
Alexandros Markopoulos <mar440@vsb.cz>
2017-04-26 23:16:51 +0200

    htfeti.cpp: hardcoded path git diff htfeti.cpp ! must be modified git diff htfeti.cpp !

--- htfeti.cpp
+++ htfeti.cpp
@@ -3,8 +3,10 @@
 #include "include/Options.hpp"
 #include "include/Cluster.hpp"
 //#include "Driver/DissectionSolver.hpp"
-int main(){ 
+int main(int argc, char *argv[]){
 
+
+    std::cout << argv[0] << std::endl;
     Options options;
     int n_subdomOnCluster = 2;
     std::string path2data = "../data";
