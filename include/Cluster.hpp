e575e0189c76cc3d032b6ecfb538b970a7650580
Alexandros Markopoulos <mar440@vsb.cz>
2017-04-28 23:42:50 +0200

    removing unsed variables K_regularized and RegMat

--- include/Cluster.hpp
+++ include/Cluster.hpp
@@ -20,11 +20,9 @@ public:
 
     /* FETI (first ...) */
     std::vector< Matrix> R;
-    std::vector< Matrix> K_regularized;
     std::vector< Matrix> Bc;
     std::vector< Matrix> Bct;
     std::vector< Matrix> Bf;
-    std::vector< Matrix> RegMat;
     /* FETI  (second ...)*/
     std::vector< Matrix> Gc;
     std::vector< Matrix > Fc_sub;
