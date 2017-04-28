e4863bc4205d77447deab5e35933c2461ddc1df6
Alexandros Markopoulos <mar440@vsb.cz>
2017-04-28 22:13:07 +0200

    updata

--- src/Cluster.cpp
+++ src/Cluster.cpp
@@ -42,10 +42,8 @@ bool reduceZeroRows, transpose, printCooOrDense;
 /*                         READING DATA (TXT)                              */
 /*                 matrices in coo format are one-based                    */
 /*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
-
-
+ 
     for (int i = 0 ; i < nS ; i++){
-
         /* K - stiffness matrix */
         string path2matrix = options.path2data+"/K"+to_string(i)+".txt";
         symmetric = 2; format = 1; offset = 1;
@@ -70,7 +68,6 @@ bool reduceZeroRows, transpose, printCooOrDense;
         symmetric = 0; format = 1; offset = 1;
         Bc[i].readCooFromFile(path2matrix,symmetric,format,offset);
 
-//        Bct[i].readCooFromFile(path2matrix,symmetric,format,offset);
         Bct[i] = Bc[i];
         reduceZeroRows = true;
         transpose = true;
@@ -84,46 +81,49 @@ bool reduceZeroRows, transpose, printCooOrDense;
 #if 1
     int i_sub = 3;
 
-    for (int i_sub = 0; i_sub < nS; i_sub++){
     double *Y = new double[K[i_sub].n_row_cmprs * R[i_sub].n_col];
-    for (int i = 0 ; i < K[i_sub].n_row_cmprs; i++){
-       Y[i] = 0;
-    }
-    K[i_sub].mv_csr(&(R[i_sub].dense[0]),Y,true,6);
+    for (int i_sub = 0; i_sub < nS; i_sub++){
+        for (int i = 0 ; i < K[i_sub].n_row_cmprs; i++){
+           Y[i] = 0;
+        }
+        K[i_sub].mv_csr(&(R[i_sub].dense[0]),Y,true,6);
 
 
 
-    double normK = 0;
-    for (int i = 0 ; i < K[i_sub].nnz; i++){
-       normK += K[i_sub].val[i] * K[i_sub].val[i];
-    }
-//    normK /= K[0].nnz;
-    double normR = 0;
-    for (int i = 0 ; i < R[i_sub].dense.size(); i++){
-       normR += R[i_sub].dense[i] * R[i_sub].dense[i];
-    }
+        double normK = 0;
+        for (int i = 0 ; i < K[i_sub].nnz; i++){
+           normK += K[i_sub].val[i] * K[i_sub].val[i];
+        }
+    //    normK /= K[0].nnz;
+        double normR = 0;
+        for (unsigned int i = 0 ; i < R[i_sub].dense.size(); i++){
+           normR += R[i_sub].dense[i] * R[i_sub].dense[i];
+        }
 
-    double normKR = 0;
-    for (int i = 0 ; i < K[i_sub].n_row_cmprs; i++){
-        for (int j = 0; j < R[i_sub].n_col; j++){
-            if (i_sub == -1){
-                if (j==0){
-                    printf("%d: ",i);
+        double normKR = 0;
+        for (int i = 0 ; i < K[i_sub].n_row_cmprs; i++){
+            for (int j = 0; j < R[i_sub].n_col; j++){
+                if (i_sub == -1){
+                    if (j==0){
+                        printf("%d: ",i);
+                    }
+                    printf(" %f\t", Y[i +  j * K[i_sub].n_row_cmprs]);
                 }
-                printf(" %f\t", Y[i +  j * K[i_sub].n_row_cmprs]);
+                normKR += Y[i +  j * K[i_sub].n_row_cmprs] *
+                            Y[i +  j * K[i_sub].n_row_cmprs];
             }
-            normKR += Y[i +  j * K[i_sub].n_row_cmprs] *
-                        Y[i +  j * K[i_sub].n_row_cmprs];
+            if (i_sub == -1){
+                printf("\n");
+            }
         }
-        if (i_sub == -1){
-            printf("\n");
-        }
-    }
 
-    printf("||K|| = %.6e, ", sqrt(normK));
-    printf("||R|| = %.6e, ", sqrt(normR));
-    printf("||KR|| /(||K|| ||R||) = %.6e \n", sqrt(normKR) / (sqrt(normK*normR)));
+        printf("||K|| = %.6e, ", sqrt(normK));
+        printf("||R|| = %.6e, ", sqrt(normR));
+        printf("||KR|| = %.6e, ",sqrt(normKR) );
+        printf("||KR|| /(||K|| ||R||) = %.6e \n", sqrt(normKR) / (sqrt(normK*normR)));
     }
+    delete [] Y;
+#endif
 
 
     i_sub = 0;
@@ -131,6 +131,7 @@ bool reduceZeroRows, transpose, printCooOrDense;
     int n_rowGc = Bc[i_sub].n_row_cmprs;
     int n_colGc = R[i_sub].n_col;
     Gc.zero_dense(n_rowGc, n_colGc );
+
     Bc[i_sub].mv_csr(&(R[i_sub].dense[0]),&(Gc.dense)[0],true ,R[i_sub].n_col);
 //
     printCooOrDense = true;
@@ -138,18 +139,16 @@ bool reduceZeroRows, transpose, printCooOrDense;
 
 
 
-#endif
-
     for (int i = 0; i < nS ; i++ ){
         printCooOrDense = true;
         K[i].printToFile("K",i,printCooOrDense);
         printCooOrDense = true;
         R[i].printToFile("R",i,printCooOrDense);
         printCooOrDense = true;
-        this->Bc[i].printToFile("Bc",i,printCooOrDense);
+        Bc[i].printToFile("Bc",i,printCooOrDense);
         printCooOrDense = true;
-        this->Bct[i].printToFile("Bct",i,printCooOrDense);
+        Bct[i].printToFile("Bct",i,printCooOrDense);
         printCooOrDense = true;
-        this->Bf[i].printToFile("Bf",i,printCooOrDense);
+        Bf[i].printToFile("Bf",i,printCooOrDense);
      }
 }
