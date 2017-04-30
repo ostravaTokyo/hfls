1343eae4166cbbc55e047d31a19fe693b0ef7e85
Alexandros Markopoulos <mar440@vsb.cz>
2017-04-30 23:41:19 +0200

    htfeti.cpp: add print 'done' in the end

--- htfeti.cpp
+++ htfeti.cpp
@@ -4,13 +4,15 @@
 #include "include/Cluster.hpp"
 //#include "Driver/DissectionSolver.hpp"
 int main(int argc, char *argv[]){
+
+
     std::cout << argv[0] << std::endl;
     Options options;
     int n_subdomOnCluster = 8;
     std::string path2data = "../data";
     options.set_values(n_subdomOnCluster, path2data);
-
+//
     Cluster cluster(options);
-    std::cout << "----------------- done -----------------\n" ;
+//    std::cout << "----------------- done -----------------\n" ;
     return 0;
 }
