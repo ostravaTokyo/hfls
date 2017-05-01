e098d499ae1646c100cf645992d7bc7d96b5a21e
Alexandros Markopoulos <mar440@vsb.cz>
2017-05-01 17:49:45 +0200

    src/Cluster.cpp: Fc_clust added (Matrix and method)

--- src/Cluster.cpp
+++ src/Cluster.cpp
@@ -121,6 +121,9 @@ bool reduceZeroRows, transpose, printCooOrDense;
         Bc[i].mult(BcKplus_dense,Fc[i],true);
         Fc[i].printToFile("Fc",i,printCooOrDense);
 
+        Fc[i].l2g_i_coo = Bc[i].l2g_i_coo;
+
+
         /* Gc - constraints matrix */
         int n_rowGc = Bc[i].n_row_cmprs;
         int n_colGc = R[i].n_col;
@@ -140,9 +143,140 @@ bool reduceZeroRows, transpose, printCooOrDense;
 
 //    Matrix::testPardiso();
 
+    createFc_clust();
+    Fc_clust.printToFile("Fc_clust",0,printCooOrDense);
 
 
     for (int i = 0 ; i < nS; i++){
        K_reg[i].FinalizeSolve(i);
     }
+}
+
+
+
+
+void Cluster::createFc_clust(){
+
+
+
+    const int nS = Fc.size();
+    int init_nnz = 0;
+    for (int i = 0; i < nS; i++){
+        init_nnz += Fc[i].nnz;
+    }
+
+
+    vector < int_int_dbl > tmpVec;
+    tmpVec.resize(init_nnz);
+
+
+/* Sorting [I, J, V]  --------------------------------------------------------*/
+    int cnt = 0;
+    int _i, _j;
+    for (int d = 0; d < nS ; d++){
+        Fc[d].DNS2COO();
+        for (int i = 0; i < Fc[d].i_coo_cmpr.size();i++){
+            _i = Fc[d].l2g_i_coo[ Fc[d].i_coo_cmpr[i] ];
+            _j = Fc[d].l2g_i_coo[ Fc[d].j_col[i] ];
+            if (_i <= _j){
+                tmpVec[cnt].I = _i;
+                tmpVec[cnt].J = _j;
+                tmpVec[cnt].V = Fc[d].val[i];
+                cnt++;
+            }
+        }
+    }
+    Fc_clust.nnz = cnt;
+    tmpVec.resize(Fc_clust.nnz);
+
+
+/* sort according to index I -------------------------------------------------*/
+    sort(tmpVec.begin(),tmpVec.end(),Matrix::cmp_int_int_I);
+/* partial sorting according to index J (per sets with same I)----------------*/
+    int startInd = 0, endInd = 1;
+    int tmpVecIprev = tmpVec[0].I;
+    for (int i = 1 ; i < tmpVec.size(); i ++){
+        if (tmpVec[i].I == tmpVecIprev){
+            endInd++;
+        }
+        if (tmpVec[i].I != tmpVecIprev || (tmpVec[i].I == tmpVecIprev &&
+                 i == tmpVec.size() - 1))
+        {
+            sort(tmpVec.begin() + startInd,
+                              tmpVec.begin() + (endInd  ),Matrix::cmp_int_int_J);
+            startInd = i;
+            endInd = i + 1;
+        }
+        tmpVecIprev = tmpVec[i].I;
+    }
+
+/* Cumulating duplicated A[I,J] elements--------------------------------------*/
+    int prevInd_I = -1;
+    int prevInd_J = -1;
+    int counter = 0;
+    int cnt_j = -1;
+    Fc_clust.l2g_i_coo.resize(tmpVec.size());
+
+    Fc_clust.i_coo_cmpr.resize(Fc_clust.nnz);
+    Fc_clust.j_col.resize(Fc_clust.nnz);
+    Fc_clust.val.resize(Fc_clust.nnz);
+
+
+    for (int i = 0 ; i < tmpVec.size(); i++){
+
+        if (prevInd_I != tmpVec[i].I){
+            Fc_clust.l2g_i_coo[counter] = tmpVec[i].I;
+            counter++;
+        }
+        if (prevInd_I == tmpVec[i].I && prevInd_J == tmpVec[i].J){
+            Fc_clust.val[cnt_j] += tmpVec[i].V;
+            Fc_clust.nnz--;
+        }
+        else {
+            cnt_j++;
+            Fc_clust.i_coo_cmpr[cnt_j] = counter - 1;
+            Fc_clust.j_col[cnt_j] = tmpVec[i].J;
+            Fc_clust.val[cnt_j] = tmpVec[i].V;
+        }
+        prevInd_I = tmpVec[i].I;
+        prevInd_J = tmpVec[i].J;
+    }
+
+    Fc_clust.format = 0;
+    Fc_clust.symmetric = 2;
+    Fc_clust.l2g_i_coo.resize(counter );
+    Fc_clust.l2g_i_coo.shrink_to_fit();
+
+    Fc_clust.i_coo_cmpr.shrink_to_fit();
+    Fc_clust.j_col.shrink_to_fit();
+    Fc_clust.val.shrink_to_fit();
+
+    Fc_clust.n_row_cmprs = counter;
+    Fc_clust.n_row = counter;
+    Fc_clust.n_col = counter;
+
+
+//    for (int i = 0; i < Fc_clust.nnz; i++){
+//        cout << Fc_clust.i_coo_cmpr[i] <<
+//          " "<< Fc_clust.l2g_i_coo[Fc_clust.i_coo_cmpr[i]] <<
+//          " "<< Fc_clust.j_col[i]      <<
+//         " " << Fc_clust.val[i] << endl;
+//    }
+
+//    for (int i = 0; i < Fc_clust.l2g_i_coo.size(); i++){
+//        cout << " l2g[" << i << "] = "<<Fc_clust.l2g_i_coo[i] << endl;
+//    }
+
+
+
+//    cout <<"Fc_clust.nnz: " << Fc_clust.nnz << endl;
+//    cout <<"counter     : " << counter << endl;
+
+    tmpVec.clear();
+    tmpVec.shrink_to_fit();
+    //Fc_clust.COO2CSR();
+
+#if 0
+        //}
+#endif
 }
