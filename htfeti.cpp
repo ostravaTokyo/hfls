c14126086365c1502fba7d4bf490ecccf1c7b41d
Alexandros Markopoulos <mar440@vsb.cz>
2017-05-01 23:35:04 +0200

    htfeti.cpp: added switch for 4 variants

--- htfeti.cpp
+++ htfeti.cpp
@@ -3,16 +3,51 @@
 #include "include/Options.hpp"
 #include "include/Cluster.hpp"
 //#include "Driver/DissectionSolver.hpp"
+
+
+using namespace std;
 int main(int argc, char *argv[]){
 
 
-    std::cout << argv[0] << std::endl;
+    cout << argv[0] << endl;
     Options options;
-    int n_subdomOnCluster = 8;
-    std::string path2data = "../data";
+
+
+    int n_subdomOnCluster;
+    string path2data;
+
+
+    int var = 4;
+
+    switch (var)
+    {
+        case (1):
+            // small variant: 8 subdom, each 3x3x3 elements
+            n_subdomOnCluster = 8;
+            path2data = "../data/";
+            break;
+        case (2):
+            // larger variant: 8 subdom, each 3x3x3 elements
+            n_subdomOnCluster = 8;
+            path2data = "../data2/";
+            break;
+        case (3):
+            // 2nd larger variant: 8 subdom, each 3x3x3 elements
+            n_subdomOnCluster = 27;
+            path2data = "../data3/";
+            break;
+        case (4):
+            // 2nd larger variant: 8 subdom, each 3x3x3 elements
+            // 'kernel' gluing
+            n_subdomOnCluster = 27;
+            path2data = "../data4/";
+            break;
+    }
+
+
     options.set_values(n_subdomOnCluster, path2data);
 //
     Cluster cluster(options);
-    std::cout << "----------------- done -----------------\n" ;
+    cout << "----------------- done -----------------\n" ;
     return 0;
 }
