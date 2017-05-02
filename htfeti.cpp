b4f9f962e24668cd004d5304236d7b500028197a
Alexandros Markopoulos <mar440@vsb.cz>
2017-05-02 09:32:03 +0200

    htfeti.cpp: description of possible variants (data, data1, data2, ...)

--- htfeti.cpp
+++ htfeti.cpp
@@ -17,27 +17,32 @@ int main(int argc, char *argv[]){
     string path2data;
 
 
-    int var = 4;
+    int var = 0;
 
     switch (var)
     {
-        case (1):
+        case (0):
             // small variant: 8 subdom, each 3x3x3 elements
             n_subdomOnCluster = 8;
             path2data = "../data/";
             break;
+        case (1):
+            // larger variant: 27 subdom, each 10x10x10 elements
+            n_subdomOnCluster = 27;
+            path2data = "../data1/";
+            break;
         case (2):
-            // larger variant: 8 subdom, each 3x3x3 elements
+            // larger variant: 8 subdom, each 6x6x6 elements
             n_subdomOnCluster = 8;
             path2data = "../data2/";
             break;
         case (3):
-            // 2nd larger variant: 8 subdom, each 3x3x3 elements
+            // 2nd larger variant: 27 subdom, each 5x5x5 elements
             n_subdomOnCluster = 27;
             path2data = "../data3/";
             break;
         case (4):
-            // 2nd larger variant: 8 subdom, each 3x3x3 elements
+            // 2nd larger variant: 27 subdom, each 5x5x5 elements
             // 'kernel' gluing
             n_subdomOnCluster = 27;
             path2data = "../data4/";
