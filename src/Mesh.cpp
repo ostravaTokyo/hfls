6cc1d9c057321c90bb41a1b2ba61b837cb569863
Alexandros Markopoulos <mar440@vsb.cz>
2017-05-10 20:28:20 +0200

    src/Mesh.cpp: change of variable name

--- src/Mesh.cpp
+++ src/Mesh.cpp
@@ -7,6 +7,7 @@ Mesh::Mesh()
 {
     nElements = 0;
     nPoints = 0;
+    nSub = 0;
 }
 
 Mesh::~Mesh()
@@ -17,15 +18,14 @@ Mesh::~Mesh()
 
 
 void Mesh::createMesh(){
-
     // - geometry setting ()
     double length[] = {6.0, 1.0, 1.0}; // corresponds to python benchmark
     //double length[] = {1.0, 1.0, 1.0};
     double radius = 1050.0;
 
     // - decomposition
-    int nElSub[] = {4,4,4};
-    int nSub[] = {2,2,1};
+    int nElSubXYZ[] = {4,4,4};
+    int nSubXYZ[] = {2,2,1};
 
 
 
@@ -36,7 +36,7 @@ void Mesh::createMesh(){
     double shift[3];
     int nElxyz_all[3];
     for (int i = 0; i < 3; i++){
-        nElxyz_all[i] = nElSub[i] * nSub[i];
+        nElxyz_all[i] = nElSubXYZ[i] * nSubXYZ[i];
     }
 
     shift[0] = 0.5 * length[0];
@@ -110,7 +110,7 @@ void Mesh::createMesh(){
     int ttt[8], _first_set[4];
     vector < int > tmp_vec (ttt, ttt + sizeof(ttt)/sizeof(int));
     int nxy= (nElxyz_all[0] + 1) * (nElxyz_all[1] + 1);
-
+    cnt = 0;
     for (int kk = 0; kk <  nElxyz_all[2]; kk++){
         for (int jj = 0; jj <  nElxyz_all[1]; jj++){
             for (int ii = 0; ii <  nElxyz_all[0]; ii++){
@@ -137,12 +137,12 @@ void Mesh::createMesh(){
     //
     cnt = 0;
     int currentIdOfMat = 0;
-    for (int KK = 0; KK <  nSub[2]; KK++){
-        for (int JJ = 0; JJ <  nSub[1]; JJ++){
-            for (int II = 0; II <  nSub[0]; II++){
-                for (int kk = KK * nElSub[2] ; kk <  (KK + 1) * nElSub[2]; kk++){
-                    for (int jj = JJ * nElSub[1]; jj <  (JJ + 1) * nElSub[1]; jj++){
-                        for (int ii = II * nElSub[0]; ii <  (II + 1) * nElSub[0]; ii++){
+    for (int KK = 0; KK <  nSubXYZ[2]; KK++){
+        for (int JJ = 0; JJ <  nSubXYZ[1]; JJ++){
+            for (int II = 0; II <  nSubXYZ[0]; II++){
+                for (int kk = KK * nElSubXYZ[2] ; kk <  (KK + 1) * nElSubXYZ[2]; kk++){
+                    for (int jj = JJ * nElSubXYZ[1]; jj <  (JJ + 1) * nElSubXYZ[1]; jj++){
+                        for (int ii = II * nElSubXYZ[0]; ii <  (II + 1) * nElSubXYZ[0]; ii++){
                             cnt = ii + jj * nElxyz_all[0] + kk * (nElxyz_all[0] * nElxyz_all[1]);
                             elements[cnt].PartitionId =  currentIdOfMat;;
                         }
@@ -152,4 +152,6 @@ void Mesh::createMesh(){
             }
         }
     }
+/*
+    */
 }
