9e771444cf27d0fb6ad852807cbc68ea0acb7177
Alexandros Markopoulos <mar440@vsb.cz>
2017-05-18 00:03:15 +0200

    size of mesh (X Y Z x y z) from command line

--- src/Options.cpp
+++ src/Options.cpp
@@ -2,7 +2,14 @@
 //
 void Options::set_values(int n_subdomOnCluster_, std::string path2data_)
 {
-//    std::cout << "in Option.cpp: n_subdomOnCluster = " << n_subdomOnCluster_ << std::endl;
+//  std::cout << "in Option.cpp: n_subdomOnCluster = " << n_subdomOnCluster_ << std::endl;
     n_subdomOnCluster = n_subdomOnCluster_;
     path2data = path2data_;
+
+    meshSetting.N[0] = 2;
+    meshSetting.N[1] = 2;
+    meshSetting.N[2] = 2;
+    meshSetting.n[0] = 2;
+    meshSetting.n[1] = 2;
+    meshSetting.n[2] = 2;
 }
--- src/Options.hpp
+++ src/Options.hpp
@@ -2,10 +2,19 @@
 #define __OPTIONS_HPP_INCLUDED
 #include <string>
 #include <iostream>
+
+class CubeMesh{
+public:
+    int N[3];
+    int n[3];
+};
+
 class Options {
     public:
         int n_subdomOnCluster;
         std::string  path2data;
         void set_values(int, std::string);
+        CubeMesh meshSetting;
+
 };
 #endif
