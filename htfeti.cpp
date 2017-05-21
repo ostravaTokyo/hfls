61387b5d0aad9515cba3cc80b8e6838ba1127f07
Alexandros Markopoulos <mar440@vsb.cz>
2017-05-21 21:18:41 +0200

    htfeti.cpp: setup pardiso and matrix dumping deactiavated

--- htfeti.cpp
+++ htfeti.cpp
@@ -14,8 +14,8 @@ int main(int argc, char *argv[]){
     double young_modulus = 10000;
     double poissons_ratio = 0.3;
     /* linear solver */
-    double pardiso_0_dissection_1 = 1;
-    int print_matrices = 1;
+    double pardiso_0_dissection_1 = 0;
+    int print_matrices = 0;
 
 // ----------------------------------------------------------------------
 // ----------------------------------------------------------------------
