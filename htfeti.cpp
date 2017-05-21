b88018d642d7eca8fff779f9c764f03af2b85982
Alexandros Markopoulos <mar440@vsb.cz>
2017-05-21 14:02:39 +0200

    htfeti.cpp: new parameters to options added

--- htfeti.cpp
+++ htfeti.cpp
@@ -8,25 +8,23 @@
 using namespace std;
 int main(int argc, char *argv[]){
 
-
+    /* data printed into */
     string path2data = "../data/";
-/* norm of matrix can be changed by following 2 parameters */
+    /* material constants */
     double young_modulus = 10000;
     double poissons_ratio = 0.3;
+    /* linear solver */
+    double pardiso_0_dissection_1 = 1;
+    int print_matrices = 1;
 
-
-//    double young_modulus = 2.1e9;
-//    double poissons_ratio = 0.4999;
-
-/* ---------------------------------------------------------*/
-/* ---------------------------------------------------------*/
-/* ---------------------------------------------------------*/
-/* ---------------------------------------------------------*/
-
+// ----------------------------------------------------------------------
+// ----------------------------------------------------------------------
 
     cout << argv[0] << endl;
     Options options;
-    options.set_values(path2data, argc, argv, young_modulus, poissons_ratio);
+    options.set_values(path2data, argc, argv,
+                        young_modulus, poissons_ratio,
+                        pardiso_0_dissection_1,print_matrices);
     Cluster cluster(options);
     cout << "----------------- done -----------------\n" ;
     return 0;
